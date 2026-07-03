f85944453afb8573dc600ceca52b4865c6b75085
Socheat <cheatgaming1111@gmail.com>
2026-07-03 20:13:35 +0700

    execise1

---
+++ doem_socheat/week1.py
@@ -0,0 +1,11 @@
+
+item_name = "Premium Wireless Headphones"  
+item_price = 149.99                         
+in_stock = True                             
+
+
+print(f"Item: {item_name} | Price: ${item_price} | In Stock: {in_stock}")
+
+in_stock = "Out of Stock"           
+
+print(f"Item: {item_name} | Price: ${item_price} | Status: {in_stock}")
