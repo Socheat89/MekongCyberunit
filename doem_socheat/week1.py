b29b523e79da1b8a63766154726dc6e3131cd595
Socheat <cheatgaming1111@gmail.com>
2026-07-03 20:19:07 +0700

    execise1

--- doem_socheat/week1.py
+++ doem_socheat/week1.py
@@ -1,11 +1,18 @@
 
-item_name = "Premium Wireless Headphones"  
-item_price = 149.99                         
-in_stock = True                             
+# item_name = "Premium Wireless Headphones"
+# item_price = 149.99
+# in_stock = True
+#
+#
+# print(f"Item: {item_name} | Price: ${item_price} | In Stock: {in_stock}")
+#
+# in_stock = "Out of Stock"
+#
+# print(f"Item: {item_name} | Price: ${item_price} | Status: {in_stock}")
 
+#excercise 2
 
-print(f"Item: {item_name} | Price: ${item_price} | In Stock: {in_stock}")
-
-in_stock = "Out of Stock"           
-
-print(f"Item: {item_name} | Price: ${item_price} | Status: {in_stock}")
+user_age = "23"
+age_in_next = int(user_age)
+next_year = age_in_next + 4
+print(next_year)
