5a98ec12359545c6ee23cce82d004ad7170998f5
chealida23-cpu <chealida23@mekong.edu.kh>
2026-07-10 20:07:48 +0700

    Exercise

---
+++ chea_lida/Exercise.py
@@ -0,0 +1,82 @@
+# The Sum of Two
+a = 15
+b = 25
+
+total = a + b
+
+print("1. Sum =", total)
+
+# Age Calculator
+current_year = 2026
+birth_year = 2006
+
+age = current_year - birth_year
+
+print("2. Age =", age)
+
+# The Grocery Bill
+apple_price = 1.50
+quantity = 5
+
+total_cost = apple_price * quantity
+
+print("3. Total Cost = $", total_cost)
+
+# Pizza Party
+slices = 20
+friends = 6
+
+each_person = slices // friends
+leftover = slices % friends
+
+print("4. Each person gets:", each_person)
+print("Left over:", leftover)
+
+# Square Area
+side = 8
+
+area = side ** 2
+
+print("5. Area =", area)
+
+# Fair Bill Split
+bill = 95.50
+friends = 4
+
+each_person = bill / friends
+
+print("6. Each person pays = $", each_person)
+
+# Average Grade
+score1 = 85
+score2 = 90
+score3 = 100
+
+average = (score1 + score2 + score3) / 3
+
+print("7. Average =", average)
+
+# Savings Goal
+weekly_save = 50
+weeks = 12
+purchase = 100
+
+money = (weekly_save * weeks) - purchase
+
+print("8. Total Savings = $", money)
+
+# Temperature Conversion
+fahrenheit = 100
+
+celsius = (fahrenheit - 32) * 5 / 9
+
+print("9. Celsius =", celsius)
+
+# Seconds to Minutes
+seconds = 500
+
+minutes = seconds // 60
+remaining_seconds = seconds % 60
+
+print("10. Minutes =", minutes)
+print("Remaining Seconds =", remaining_seconds)
